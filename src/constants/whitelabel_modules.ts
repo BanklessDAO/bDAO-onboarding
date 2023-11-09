f04d84ce452d9df84f2773bde3a563d9a2883243
Didier Krux <didier@krux.co>
2023-11-09 16:48:37 +0000

    restore config

--- src/constants/whitelabel_modules.ts
+++ src/constants/whitelabel_modules.ts
@@ -1,5 +1,56 @@
 import { ModuleType } from 'entities/module'
 
-const MODULES: ModuleType[] = []
+const MODULES: ModuleType[] = [
+  {
+    name: 'Constitution',
+    moduleImageLink: '/BanklessDAO/module/constitution-ad01c516.jpg',
+    socialImageLink: '/BanklessDAO/module/constitution-social-c257a061.jpg',
+    description: 'This module covers the BanklessDAO constitution.',
+    slug: 'constitution',
+    moduleId: 'fe8c38b6b1244b84ae8d185c9a8587b7',
+    parentModule: null,
+    subModules: []
+  },
+  {
+    name: 'PM guild',
+    moduleImageLink: undefined,
+    socialImageLink: undefined,
+    description: 'This module covers BanklessDAO’s Intro to Web3 PM',
+    slug: 'pm-guild',
+    moduleId: '1d3abdbb01314335a76b872af851fad5',
+    parentModule: null,
+    subModules: []
+  },
+  {
+    name: 'Bankless Card',
+    moduleImageLink: undefined,
+    socialImageLink: undefined,
+    description: 'This module covers onboarding to the Bankless Card project',
+    slug: 'bankless-card',
+    moduleId: '3dd60d1ede6a40d89ec961080ab21e62',
+    parentModule: null,
+    subModules: []
+  },
+  {
+    name: 'Podcast Hatchery',
+    moduleImageLink: '/BanklessDAO/module/podcast-hatchery-581d935b.png',
+    socialImageLink: '/BanklessDAO/module/podcast-hatchery-social-b9a5b9e8.png',
+    description: 'Podcast Hatchery’s Incubator Course',
+    slug: 'podcast-hatchery',
+    moduleId: 'c842df7f5e8348a0b3d0112c87fd9313',
+    parentModule: null,
+    subModules: []
+  },
+  {
+    name: 'Bankless DeSci',
+    moduleImageLink: undefined,
+    socialImageLink: undefined,
+    description: 'This module covers onboarding to Bankless DeSci',
+    slug: 'bankless-desci',
+    moduleId: '6602124d937a4e4b97312e51a53141d2',
+    parentModule: null,
+    subModules: []
+  }
+]
 
 export default MODULES
